508d685b2b114e0184687ec116c454adf4914d57
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2026-01-14 21:45:30 +0100

    Component - menu

---
+++ app/components/menu.vue
@@ -0,0 +1,9 @@
+<template>
+    <nav class="mb-2">
+        <ul class="pt-2">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+        </ul>
+    </nav>
+</template>
