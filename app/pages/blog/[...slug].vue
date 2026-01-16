85cf560028349dd0fb6118f4eee4fe7f4cf4df32
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2026-01-16 20:47:19 +0100

    Tailwind typograhy - make NUXT content pages styled with prose

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -7,7 +7,7 @@ const { data: page } = await useAsyncData(route.path, () => {
 </script>
 
 <template>
-    <article class="bg-red-500 dark:bg-amber-300 dark:text-black p-5 rounded-lg">
+    <article class="bg-red-500 dark:bg-amber-300 dark:text-black p-5 rounded-lg prose dark:prose-invert">
         <ContentRenderer v-if="page" :value="page" />
     </article>
 </template>
