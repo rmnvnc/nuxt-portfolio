5866c583dab427c7d1effdb52ee23fdee1b66754
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2026-01-18 17:04:57 +0100

    NUXT Content - better SEO for about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -4,6 +4,22 @@ const route = useRoute()
 const { data: page } = await useAsyncData(route.path, () => {
     return queryCollection('content').path(route.path).first()
 })
+
+const title = page.value?.seo?.title || page.value?.title
+const description = page.value?.seo?.description || page.value?.description
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description
+})
+
+defineOgImageComponent('NuxtSeo', {
+    title: title,
+    description: description
+})
+
 </script>
 
 <template>
--- content/about.md
+++ content/about.md
@@ -1,3 +1,11 @@
+---
+title: 'About title'
+description: 'About description text'
+seo: {
+    title: 'Im SEO title from .md file'
+}
+---
+
 # Im about page from MD
 
 Basic text from MD markdown
