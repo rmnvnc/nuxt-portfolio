bb1b416be34dfbb7309f7181d4ecc0a4dd90a92e
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2026-01-18 16:40:54 +0100

    NUXT Content - move project page to .md

--- app/components/content/ProjectList.vue
+++ app/components/content/ProjectList.vue
@@ -8,7 +8,6 @@
 
 <template>
     <div>
-        Im project list
         <section v-if="pending">
             <svg class="mx-auto size-8 animate-spin text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,5 +1,27 @@
+
+<script setup lang="ts">
+const route = useRoute()
+const { data: page } = await useAsyncData(route.path, () => {
+    return queryCollection('content').path(route.path).first()
+})
+
+useSeoMeta({
+    title: page.value?.title,
+    description: page.value?.description,
+    ogTitle: page.value?.title,
+    ogDescription: page.value?.description,
+    twitterTitle: page.value?.title,
+    twitterDescription: page.value?.description,
+})
+
+defineOgImageComponent('NuxtSeo', {
+    title: page.value?.title,
+    description: page.value?.description
+})
+</script>
+
 <template>
-    <div>
-        <ProjectList />
-    </div>
+    <article>
+        <ContentRenderer v-if="page" :value="page" />
+    </article>
 </template>
