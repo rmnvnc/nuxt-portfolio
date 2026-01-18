7a89ed4f642db51db07c3c9bb9f818c038aed783
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2026-01-18 13:10:59 +0100

    NUXT Content - blog listing

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+    const route = useRoute()
+    const { data: posts } = await useAsyncData(route.path, () => {
+        return queryCollection('content').select('id', 'path', 'title').all()
+    })
+</script>
+
 <template>
-    Blog page
+    <div>
+        <article v-for="post in posts" :key="post.id">
+            <NuxtLink :to="post.path" class="underline hover:underline-offset-4">
+                {{ post.title }}
+            </NuxtLink>
+        </article>
+    </div>
 </template>
